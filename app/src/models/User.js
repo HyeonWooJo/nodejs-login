e44eb6fee5e1aaf3f8538f4ffce1d4c2f9b70b52
HyeonWooJo <HyeonWooJo@github.com>
2022-08-25 20:33:49 +0900

    Add: MySQL 연동 & dotenv로 환경변수 변등

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -9,18 +9,18 @@ class User{
 
     async login() {
         const client = this.body;
-        const {id, psword} = await UserStorage.getUserInfo(client.id);
-        // await: Promise를 반환할 때만 사용가능하다. 이는 await 오른쪽에
-        // 작성된 로직이 모두 실행되기까지 기다린 후에 왼쪽의 로직이 실행되도록 하는 것이다.
-        // await는 async 안에서만 사용할 수 있다.
-
-        if (id) {
-            if (id === client.id && psword ==client.psword){
-                return { success: true};
+        try{
+            const {id, psword} = await UserStorage.getUserInfo(client.id);
+            if (id) {
+                if (id === client.id && psword ==client.psword){
+                    return { success: true};
+                }
+                return { success: false, msg: '비밀번호가 틀렸습니다.'};
             }
-            return { success: false, msg: '비밀번호가 틀렸습니다.'};
+            return { success: false, msg: '존재하지 않는 아이디입니다.'};
+        } catch{err} {
+            return { success: false, msg: err };
         }
-        return { success: false, msg: '존재하지 않는 아이디입니다.'};
     }
 
     async register() {
@@ -29,8 +29,7 @@ class User{
             const response = await UserStorage.save(client);
             return response;
         } catch (err){
-            const a = { success: false, msg: err };
-            return a;
+            return { success: false, msg: err };
         }
     }
 }
